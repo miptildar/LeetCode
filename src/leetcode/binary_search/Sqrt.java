1bec7713ceff3c9204daed8e1c32268871aa86b1
Ildar <i.zaripov@arammeem.com>
2025-06-26 09:37:29 +0300

    Binary search problems

---
+++ src/leetcode/binary_search/Sqrt.java
@@ -0,0 +1,42 @@
+package leetcode.binary_search;
+
+public class Sqrt {
+
+    public int mySqrt(int x) {
+
+        if (x == 0 || x == 1) return x;
+
+        if (x == 2 || x == 3) return 1;
+
+        int low = 0;
+        int high = x;
+
+        if (x > 46341) {
+            high = 46341;
+        }
+
+        while (high - low != 1) {
+            int mid = low + (high - low) / 2;
+
+            long midSquare = mid * mid;
+            if (midSquare == x) return mid;
+
+            if (midSquare > x) {
+                high = mid;
+                continue;
+            }
+
+            if (high * high == x) {
+                return high;
+            }
+
+            if (midSquare < x) {
+                low = mid;
+                continue;
+            }
+        }
+
+        return low;
+    }
+
+}
---
+++ src/leetcode/hash_table/IntersectionOfTwoArrays.java
@@ -0,0 +1,24 @@
+package leetcode.hash_table;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class IntersectionOfTwoArrays {
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> first = new HashSet<>();
+        for (int i=0; i<nums1.length; i++) first.add(nums1[i]);
+
+        List<Integer> list = new ArrayList<>();
+        for (int i=0; i<nums2.length; i++) {
+            if (first.contains(nums2[i])) list.add(nums2[i]);
+        }
+
+        int[] result = new int[list.size()];
+        for (int i=0; i<list.size(); i++) result[i] = list.get(i);
+        return result;
+    }
+
+}
