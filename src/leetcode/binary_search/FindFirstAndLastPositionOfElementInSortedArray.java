245fb97b880e994687fdc6397a6eedef80545717
Ildar <i.zaripov@arammeem.com>
2025-07-01 23:14:59 +0300

    Binary search problems

---
+++ src/leetcode/binary_search/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -0,0 +1,42 @@
+package leetcode.binary_search;
+
+public class FindFirstAndLastPositionOfElementInSortedArray {
+
+    public static void main(String[] args) {
+        new FindFirstAndLastPositionOfElementInSortedArray().searchRange(new int[]{1,3}, 1);
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        if (nums.length == 0) return new int[]{-1,-1};
+
+        int index = search(nums, target);
+        if (index == -1) return new int[]{-1,-1};
+
+        int left = index;
+        int right = index;
+
+        while (true) {
+            if (left > 0 && nums[left - 1] == target) left--;
+            else if (right < nums.length - 1 && nums[right + 1] == target) right++;
+            else break;
+        }
+
+        return new int[] {left, right};
+
+    }
+
+    int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        while (left <= right && left < nums.length && right >= 0) {
+
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            if (target < nums[mid]) right = mid - 1;
+            else left = mid + 1;
+        }
+
+        return -1;
+    }
+
+}
